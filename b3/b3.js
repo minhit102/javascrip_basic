7c251a1ae7487e4b3fbfcd4dd51fb21111fcd30d
oop <minhit102k66@gmail.com>
2023-08-18 11:04:46 +0700

    haha

--- b3/b3.js
+++ b3/b3.js
@@ -1,6 +1,9 @@
-console.log("Minh")
-let a, b, c;
-a = 5;
-b = 6;
-c = a + b;
-document.getElementById("demo").innerHTML = c + 1;
+let a = "Minh";
+let b = "Quang";
+let c = a+b;
+let text = 'We are the so-called "Vikings" from the north."'
+let d = "qwertyuiopasdfghjkl  zxcvbnm        ";
+
+document.getElementById("demo").innerHTML = text;
+console.log(c);
+console.log(d.trim().length);
