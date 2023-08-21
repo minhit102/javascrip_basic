2cd31328386e95cced6ba0d5ff9f5b8f4cee099e
oop <minhit102k66@gmail.com>
2023-08-21 12:11:03 +0700

    bai12

---
+++ b3/b4.js
@@ -0,0 +1,8 @@
+document.getElementById('demo1').innerHTML = "Minh 2k3";
+let arr = [0,1,3,4,6,6];
+console.log(`first : ${arr[2]}`, arr);
+let arr1 = [
+    {name: 'Minh' , age : 20},
+    {name: 'Gia huy' , age : 8}
+]
+console.log(`first : ${arr[2]}`, arr1);
