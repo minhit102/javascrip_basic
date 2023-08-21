285b317a65e6866e3386a37595834dd867cdc653
oop <minhit102k66@gmail.com>
2023-08-21 11:44:16 +0700

    b12

--- b3/b3.js
+++ b3/b3.js
@@ -1,3 +1,13 @@
-let str = "Toi ten la Hoang Trong Minh 20215427";
-let myArr = str.split("");
-document.getElementById("demo1").innerHTML = myArr[7];
+let obj = {
+    name : 'Minh1',
+    address : 'Can Loc',
+    func1 : function(){
+        document.getElementById("demo1").innerHTML = "Minhit102" ;
+    }
+};
+
+console.log("What is your name ?" , `My name is : ${obj.name} ` );
+obj.func1();
+obj['name'] = "Hoang Trong Minh 1 ";
+let ten = "name"
+document.getElementById('demo2').innerHTML = obj[ten];
